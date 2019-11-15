1143f6a99bc8dc36f87448210f475fdaf0a5e6ef
François Kooman <fkooman@tuxed.net>
2019-11-15 17:25:00 +0100

    implement metadata cache

--- bin/generate_discovery_files.php
+++ bin/generate_discovery_files.php
@@ -6,18 +6,26 @@ use VPN\Discovery\MetadataParserAll;
 
 // generate the eduVPN application discovery files
 // we need a mapping from IdP to server
+@\mkdir(\dirname(__DIR__).'/cache', 0711, true);
 
 $orgList = ['orgList' => []];
-
 $idpServerMapping = [];
-
 $mappingData = \json_decode(\file_get_contents('mapping.json'), true);
 foreach ($mappingData as $baseUrl => $instanceData) {
     $metadataUrlList = $instanceData['metadata_url'];
     if (0 !== \count($metadataUrlList)) {
         foreach ($metadataUrlList as $metadataUrl) {
             try {
-                $md = new MetadataParserAll($metadataUrl);
+                $metadataLocalFile = \dirname(__DIR__).'/cache/'.\urlencode($metadataUrl);
+                if (!@\file_exists($metadataLocalFile)) {
+                    if (false === $metadataContent = @\file_get_contents($metadataUrl)) {
+                        throw new RuntimeException(\sprintf('unable to fetch "%s"', $metadataUrl));
+                    }
+                    if (false === @\file_put_contents($metadataLocalFile, $metadataContent)) {
+                        throw new RuntimeException(\sprintf('unable to write "%s"', $metadataLocalFile));
+                    }
+                }
+                $md = new MetadataParserAll($metadataLocalFile);
                 $idpInfoList = $md->get();
                 foreach ($idpInfoList as $idpInfo) {
                     $entityId = $idpInfo->getEntityId();

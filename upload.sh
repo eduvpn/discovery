33791c1e988689fcf7fbab24ab57a12f77472073
François Kooman <fkooman@tuxed.net>
2020-03-19 11:05:48 +0100

    convert minisign sig format to signify

--- upload.sh
+++ upload.sh
@@ -2,4 +2,14 @@
 rm -rf output/*
 php bin/generate.php
 minisign -S -m output/*.json
+
+# rename .minisig to .sig and keep only the two first lines to make them the 
+# same as signify
+for I in $(ls output/*.minisig)
+do 
+    BN=$(basename ${I} .minisig)
+    cat ${I} | head -2 > output/${BN}.sig
+    rm ${I}
+done
+
 rsync -avzuh -e ssh output/ argon.tuxed.net:/var/www/html/fkooman/eduVPN/discovery/v2 --progress --exclude ".git"

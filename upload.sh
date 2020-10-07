b8c5c72ccf9c85a80f060201afbbadf01eba043e
François Kooman <fkooman@tuxed.net>
2020-10-07 14:02:18 +0200

    print name of server when rsyncing

--- upload.sh
+++ upload.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+
+# legacy to spion.eduvpn.nl (@SURF)
 scp out/* spion.eduvpn.nl:/var/www/html/web/disco.eduvpn.org/v2
 
 # CDN
 SERVER_LIST="tromso-cdn.eduroam.no ifi2-cdn.eduroam.no"
 for SERVER in ${SERVER_LIST}; do
+    echo "${SERVER}..."
 	rsync -e ssh -rtO --delete out/ "${SERVER}:/srv/disco.eduvpn.org/www/v2/" || echo "FAIL ${SERVER}"
 done

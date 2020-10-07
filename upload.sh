8d579a5eb249aa1d23014cb9fc31de35328738c9
François Kooman <fkooman@tuxed.net>
2020-10-07 13:39:27 +0200

    re-enable CDN

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 scp out/* spion.eduvpn.nl:/var/www/html/web/disco.eduvpn.org/v2
 
 # CDN
-#SERVER_LIST="tromso-cdn.eduroam.no ifi2-cdn.eduroam.no"
-#for SERVER in ${SERVER_LIST}; do
-#	rsync -e ssh -rtO --delete out/ "${SERVER}:/srv/disco.eduvpn.org/www/v2/" || echo "FAIL ${SERVER}"
-#done
+SERVER_LIST="tromso-cdn.eduroam.no ifi2-cdn.eduroam.no"
+for SERVER in ${SERVER_LIST}; do
+	rsync -e ssh -rtO --delete out/ "${SERVER}:/srv/disco.eduvpn.org/www/v2/" || echo "FAIL ${SERVER}"
+done

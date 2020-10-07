35e91d6309a9688fdb6ebf65465eb30b7111bb59
François Kooman <fkooman@tuxed.net>
2020-10-07 12:11:48 +0200

    add disco CDN

--- upload.sh
+++ upload.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 scp out/* spion.eduvpn.nl:/var/www/html/web/disco.eduvpn.org/v2
+
+# disco.eduvpn.org CDN
+SERVER_LIST="tromso-cdn.eduroam.no ifi2-cdn.eduroam.no"
+for SERVER in ${SERVER_LIST}; do
+	rsync -e 'ssh -o PasswordAuthentication=no' -rtO --delete out/ "${SERVER}:/srv/disco.eduvpn.org/www/v2/" || echo "FAIL ${SERVER}"
+done

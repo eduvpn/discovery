aae524233bf081190cd11d50b5d501021a7ee5a7
François Kooman <fkooman@tuxed.net>
2020-10-13 11:22:02 +0200

    drop legacy update location

--- upload.sh
+++ upload.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# legacy to spion.eduvpn.nl (@SURF)
-scp out/* spion.eduvpn.nl:/var/www/html/web/disco.eduvpn.org/v2
-
 # CDN
 SERVER_LIST="tromso-cdn.eduroam.no ifi2-cdn.eduroam.no"
 for SERVER in ${SERVER_LIST}; do

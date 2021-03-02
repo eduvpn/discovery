cab28fbaaa01962d6f3966927d61cec86a2295dd
François Kooman <fkooman@tuxed.net>
2021-03-02 08:48:42 +0100

    verify JSON syntax of discovery file

--- generate.sh
+++ generate.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# verify JSON file
+if ! python -mjson.tool server_list.json > /dev/null;
+then
+	echo "ERROR in server_list.json!"
+	exit 1
+fi
+
 php bin/generate.php
 rm -f out/*.gz
 #gzip -9k out/server_list.json out/organization_list.json
